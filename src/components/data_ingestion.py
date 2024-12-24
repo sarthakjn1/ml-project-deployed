a7fdbb5981ab262bb16e966e7f05ef3eee534fca
Sarthak Nagapurkar <nsarthak99@gmail.com>
2024-12-24 18:24:33 +0530

    Prediction Pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -49,15 +49,15 @@ class DataIngestion:
             raise CustomException(e, sys)
         
 
-if __name__=="__main__":
-    obj = DataIngestion()
-    train_data, test_data = obj.initiate_data_ingestion()
+# if __name__=="__main__":
+#     obj = DataIngestion()
+#     train_data, test_data = obj.initiate_data_ingestion()
 
-    data_transformation = DataTransformation()
-    train_arr, test_arr, preprocessor_obj_path = data_transformation.initiate_data_transformation(train_data, test_data)
+#     data_transformation = DataTransformation()
+#     train_arr, test_arr, preprocessor_obj_path = data_transformation.initiate_data_transformation(train_data, test_data)
 
-    model_trainer = ModelTrainer()
-    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+#     model_trainer = ModelTrainer()
+#     model_trainer.initiate_model_trainer(train_arr, test_arr)
 
 
 
